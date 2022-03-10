8c5ec465edcf7bc0e01b22e1a6636a057a458bf0
lizarbpachecoventurim <lizarb.pacheco@mobilibus.com.br>
2022-03-10 07:42:02 -0300

    Co-authored-by: André Luiz Gomes Pereira <AndreLuizGomesPereira@users.noreply.github.com> Co-authored-by: Emerson Santana <emersonfidelon@gmail.com>

--- cypress/integration/store/access_site.spec.js
+++ cypress/integration/store/access_site.spec.js
@@ -13,7 +13,18 @@ describe("Site Automation", () => {
     })
 
     it('Criar conta', () => {
+        cy.get('#email_create').type('teste@beedoo.com.br')
+        cy.get('#SubmitCreate > span').click({force:true})
+        cy.get('div.account_creation:nth-child(1) > h3:nth-child(1)').contains('Your personal information')
+    })
 
+    it('Preencher dados da conta',() => {
+        cy.get('#customer_firstname').type('Teste')
+        cy.get('#customer_lastname').type('sobrenome teste')
+        cy.get('#passwd').type('12345')
+        cy.get('#firstname').type('teste adress')
+
+        
     })
 
 });
